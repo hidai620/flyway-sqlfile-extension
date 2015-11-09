17ce222f0c80e053f2cac0a70102c04405f0479e
Yoshiaki Hidai <hidai.620@gmail.com>
2015-11-10 02:43:37 +0900

    [Change] extends AutoPlugin

--- project/flyway.sbt
+++
@@ -1,3 +0,0 @@
-addSbtPlugin("org.flywaydb" % "flyway-sbt" % "3.2.1")
-
-resolvers += "Flyway" at "http://flywaydb.org/repo"
--- src/main/scala/com/github/hidai620/FlywaySqlfileExtensionPlugin.scala
+++ src/main/scala/com/github/hidai620/FlywaySqlfileExtensionPlugin.scala
@@ -2,28 +2,52 @@ package com.github.hidai620
 
 import sbt._
 import Keys._
-import java.text.SimpleDateFormat
 import java.io.File
 import java.util.Date
+import java.text.SimpleDateFormat
 import org.flywaydb.sbt.FlywayPlugin._
 
-object FlywaySqlfileExtension extends Plugin {
+object FlywaySqlFileExtension extends sbt.AutoPlugin {
 
-  val flywayAddSqlFile = inputKey[Unit]("flywayのSQLファイルを作成する")
+  object autoImport {
+    val flywaySqlFileAdd = inputKey[Unit]("Add flyway versioned sql file")
+    val flywaySqlFileTimeFormat = settingKey[String]("flyway migration sql file name timestamp format")
+  }
 
-  val flywaySqlFileTimeFormat = settingKey[String]("flyway migration sql file name timestamp formt")
+  val migrationSqlFileDirName = "src/main/resources/db/migration"
 
-  override val settings = Seq(
+
+  import autoImport._
+  override def projectSettings:Seq[Def.Setting[_]] = Seq(
     flywaySqlFileTimeFormat := "yyyy.MM.dd.HH.mm.ss",
-
-    flywayAddSqlFile := {
+    flywaySqlFileAdd := {
       val args: Seq[String] = Def.spaceDelimited("<arg>").parsed
-      val formatter = new SimpleDateFormat(flywaySqlFileTimeFormat.value)
-      val sqlFileName = s"${flywaySqlMigrationPrefix.value}${formatter.format(new Date)}${flywaySqlMigrationSeparator.value}${args.mkString("_")}${flywaySqlMigrationSuffix.value}"
-      val migrateSqlFileDirName = "src/main/resources/db/migration"
-      val sqlFilePath = s"${baseDirectory.value}/${migrateSqlFileDirName}/${sqlFileName}"
-      new File(sqlFilePath).createNewFile
+      val timestamp = new SimpleDateFormat(flywaySqlFileTimeFormat.value).format(new Date)
+      val description = args.mkString("_")
+
+      val sqlFileName = s"${flywaySqlMigrationPrefix.value}${timestamp}${flywaySqlMigrationSeparator.value}"+
+        s"${description}${flywaySqlMigrationSuffix.value}"
+
+      val sqlFileDirPath = s"${baseDirectory.value}/${migrationSqlFileDirName}"
+
+      createSqlFileDirectory(sqlFileDirPath)
+      createSqlFile(sqlFileDirPath, sqlFileName)
     }
   )
 
+  private def createSqlFile(sqlFileDirPath:String, sqlFileName:String):Boolean = {
+    val sqlFilePath = s"${sqlFileDirPath}/${sqlFileName}"
+    new File(sqlFilePath).createNewFile
+  }
+
+  private def createSqlFileDirectory(sqlFileDirPath:String):Boolean = {
+    val sqlFileDir = new File(sqlFileDirPath)
+    if (!sqlFileDir.exists) {
+      sqlFileDir.mkdirs
+    } else {
+      true
+    }
+  }
+
+  override def trigger = allRequirements
 }
